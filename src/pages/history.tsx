5423640906c77dbc6bb7106dc3bf3d964e60f835
jacker <>
2020-11-26 23:41:23 +0800

    增加接口请求时间

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -15,6 +15,31 @@ interface Props {
 @connect([`historyData`])
 class History extends React.PureComponent<Props> {
 
+    private formatTime = (time: number) => {
+        const date = new Date(time);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const second = date.getSeconds();
+
+        const left: string = [year, month, day].map(
+            (value: any) => this.completion(value)
+        ).join(`-`);
+
+        const right: string = [hour, minute, second].map(
+            (value: any) => this.completion(value)
+        ).join(`:`);
+
+        return [left, right].join(` `);
+    }
+
+    private completion = (code: any) => {
+        code = `${code}`;
+        return code.length < 2 ? `0${code}` : code;
+    }
+
     get historyList() {
         const {
             historyData
@@ -33,15 +58,16 @@ class History extends React.PureComponent<Props> {
                 {this.historyList.map((item) => {
                     const {
                         success,
-                        startTime,
-                        endTime,
-                        jsonData
+                        jsonData,
+                        endTime = 0,
+                        startTime = 0
                     } = item;
 
                     return (
                         <div key={item.id} className={styles.item}>
                             <div className={styles.header}>
-                                <div>请求耗时:{endTime - startTime} ms</div>
+                                <div>请求时间:{this.formatTime(startTime)}</div>
+                                <div>请求耗时:{endTime - startTime}ms</div>
                                 <div>请求状态: {success ? `成功` : `失败`}</div>
                             </div>
                             <div className={styles.content}>
